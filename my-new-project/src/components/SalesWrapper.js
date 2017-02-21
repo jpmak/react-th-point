6e3f5518cae900b946f9a81e012e4c4babaf8ae0
mark <332178065@qq.com>
2017-02-22 00:29:35 +0800

    1

--- my-new-project/src/components/SalesWrapper.js
+++ my-new-project/src/components/SalesWrapper.js
@@ -18,11 +18,12 @@ var SalesWrapper = React.createClass({
                     loading: false,
                     data: value
                 });
-            }
-            $("img.lazy").show().lazyload({
-                container: $("#sales-wrapper"),
+                       $("img.lazy").show().lazyload({
+                container: $("#SalesWrapper"),
                 effect: "fadeIn"
             });
+            }
+     
         }.bind(this));
     },
     render: function() {
@@ -30,12 +31,33 @@ var SalesWrapper = React.createClass({
             return <span > Loading... </span>;
         } else {
             var Sales = this.state.data.goods_list;
+            //     var sales_top = '';
+            // switch (Sales.length) {
+            //             case 0:
+            //                 sales_top = 't1';
+            //                 break;
+            //             case 1:
+            //                 sales_top = 't2';
+            //                 break;
+            //             case 2:
+            //                 sales_top = 't3';
+            //                 break;
+            //             case 3:
+            //                 sales_top = '';
+            //                 break;
+            //         }
+            //         console.log(sales_top);
             var SalesList = Sales.map(function(repo, index) {
-                return (<li><a href="Exchange-goods-{repo.item_id}.html"><div className="info-img"><div class="top">{repo.sales_top}</div><img alt="" className="lazy"   data-original= {repo.list_image} /></div><div className="info-bar"><div className="pro-title">{repo.goods_name}</div><div className="e-numb"><span className="e-price"><em>{repo.item_price}</em>积分</span></div></div></a> </li>);
+              
 
+                   
+                return (
+
+                    <li key={index}><a href={'Exchange-goods-'+repo.item_id+'.html'}><div className="info-img"><div className={'top' + sales_top}>{repo.sales_top}</div><img alt="" className="lazy"   data-original= {repo.list_image} /></div><div className="info-bar"><div className="pro-title">{repo.goods_name}</div><div className="e-numb"><span className="e-price"><em>{repo.item_price}</em>积分</span></div></div></a> </li>);
             });
+
             return (
-                <div id="scroller" class="list">
+                <div id="scroller" className="list">
                         <ul>
                         {SalesList}
                         </ul>
