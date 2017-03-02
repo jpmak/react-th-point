6b59ad1e9941e1db4e553c5fba27a00c0a8e30c9
mark <332178065@qq.com>
2017-03-02 23:57:35 +0800

    1

--- my-new-project/src/components/JsCate.js
+++ my-new-project/src/components/JsCate.js
@@ -14,9 +14,9 @@ var JsPrduct = React.createClass({
         return (
             <div className="app-pd-wp">
                 <div className="app-pd-list">
-                        <ul>
-                            {this.props.data}
-                        </ul>
+               <ul dangerouslySetInnerHTML= {{__html:this.props.data}}>
+                           
+                   </ul> 
                     </div>
                     </div>
         )
@@ -29,7 +29,7 @@ var App = React.createClass({
             loading: true,
             error: null,
             data: null,
-            message: '',
+            message:'',
             data_p: null
         };
     },
@@ -46,20 +46,20 @@ var App = React.createClass({
                     $('.choose-items-wp li').first().addClass('act');
                     // 初始化
 
-                    var nav_w = $(".app-scroller li").first().width();
-                    var fl_w = $(".app-scroller").width();
-                    var flb_w = $(".app-scroller-wrap").width();
-                    $(".choose-items-wp p").width(nav_w);
-                    $(".app-scroller li").on("click", function() {
+                    var nav_w = $('.app-scroller li').first().width();
+                    var fl_w = $('.app-scroller').width();
+                    var flb_w = $('.app-scroller-wrap').width();
+                    $('.choose-items-wp p').width(nav_w);
+                    $('.app-scroller li').on('click', function() {
                         nav_w = $(this).width();
-                        $(".choose-items-wp p").stop(true);
-                        $(".choose-items-wp p").animate({
+                        $('.choose-items-wp p').stop(true);
+                        $('.choose-items-wp p').animate({
                             left: $(this).position().left
                         }, 300);
-                        $(".choose-items-wp p").animate({
+                        $('.choose-items-wp p').animate({
                             width: nav_w
                         });
-                        var fn_w = ($(".app-scroller-wrap").width() - nav_w) / 2;
+                        var fn_w = ($('.app-scroller-wrap').width() - nav_w) / 2;
 
                         var fnl_l;
                         var fnl_x = parseInt($(this).position().left);
@@ -71,11 +71,11 @@ var App = React.createClass({
                         } else {
                             fnl_l = fn_w - fnl_x;
                         }
-                        $(".app-scroller").animate({
-                            "left": fnl_l
+                        $('.app-scroller').animate({
+                            'left': fnl_l
                         }, 300);
                         sessionStorage.left = fnl_l;
-                        var c_nav = $(this).find("a").text();
+                        var c_nav = $(this).find('a').text();
                         // navName(c_nav);
                     });
                     // 初始化
@@ -86,31 +86,30 @@ var App = React.createClass({
         // this.setState({
         // })
 
-        this.setState({
-            message: function() {
-                $.ajax({
-                    url: 'http://dev.thgo8.com/?g=WapSite&c=Exchange&a=get_cate_goods',
-                    type: 'POST',
-                    dataType: 'json',
-                    data: {
-                        'cate_id': event,
-                        'page': 0
-                    },
-                    success: function(value) {
-                        this.setState({
-                            loading: false,
-                            data: value
-                        });
-                        $(".app-pd-list img.lazy").show().lazyload({
-                            placeholder: "/src/images/f-bg.gif",
-                            skip_invisible: false,
-                            effect: "fadeIn",
-                            threshold: 0,
-                        });
-                    }.bind(this),
-                });
-            }
-        });
+   
+const _this = this;
+    $.ajax({
+        url: 'http://dev.thgo8.com/?g=WapSite&c=Exchange&a=get_cate_goods',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            'cate_id': event,
+            'page': 0
+        },
+
+        success: function(data, textStatus, xhr) {
+            let cate_listHtml = '';
+            if (data.status) {
+                       for (var i = 0; i < data.goods_list.length; i++) {
+                    cate_listHtml += '  <li><a href="Exchange-goods-' + data.goods_list[i].item_id + '.html"><div class="info-img"><img alt="" class="lazy" data-original="' + data.goods_list[i].list_image + '"></div><div class="info-bar"><div class="pro-title">' + data.goods_list[i].goods_name + '</div><div class="e-numb"><span class="e-price"><em>' + data.goods_list[i].item_price + '</em>积分</span></div></div></a></li>';
+                }
+      _this.setState({
+            message:cate_listHtml
+        })
+            } else {
+        }
+    }
+    });
     },
     render: function() {
         if (this.state.loading) {
@@ -120,8 +119,10 @@ var App = React.createClass({
             var self = this;
             var CateList = Cates.map(function(Cate, index, event) {
                 // var indexS = index.shift();
+         var message_id=Cates[0].cate_id;
+
                 return (
-                    <li key={index} id={Cate.cate_id} className="1" onClick={self.handleClick.bind(self,Cate.cate_id)}><a><span>{Cate.cate_name}</span></a></li>
+                    <li key={index} id={Cate.cate_id}  onClick={self.handleClick.bind(self,Cate.cate_id)}><a><span>{Cate.cate_name}</span></a></li>
                 );
             });
             return (
@@ -144,6 +145,7 @@ var App = React.createClass({
 
 
 })
+
 
 export {
     App,
